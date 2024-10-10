9682184424e39f408d529ece5336fabcf7d3d92f
Cassie Asher <cassasher@gmail.com>
2024-10-10 00:16:53 -0600

    Corrected BEM issues and responsive design

--- scripts/validation.js
+++ scripts/validation.js
@@ -75,7 +75,7 @@ function enableValidation(options) {
 const config = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
-  submitButtonSelector: ".modal__button",
+  submitButtonSelectorSelector: ".modal__button",
   inactiveButtonClass: "modal__button_disabled",
   inputErrorClass: "modal__input_type_error",
   errorClass: "modal__error_visible",
